0298da5c366295bbce69f3ec710edb9bbcc0357d
Hyo-jung Lee <leeexpert@cau.ac.kr>
2022-08-09 17:09:30 +0900

    220809

--- calculation.js
+++ calculation.js
@@ -6,7 +6,6 @@ function calculate(){
     const insertConc=document.getElementById('insertConc').value;
     const vectorMass=document.getElementById('vectorMass').value;
 
-    alert("계산 시작! 목표비율 : "+ratio+"벡터길이 : "+vectorLength+"인서트길이 : "+insertLength+"벡터 농도 : "+vectorConc+" 인서트농도 : "+insertConc+"벡터 매스 : "+vectorMass)
     const result=(((vectorLength/insertLength)*ratio)/insertConc)*(vectorMass/100);
 
     const vectorVol=document.getElementById('vectorVol');
